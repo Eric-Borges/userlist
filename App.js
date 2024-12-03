2de6debcabdbd4ab166d5db3b2026501c98369d9
borges <147554727+Eric-Borges@users.noreply.github.com>
2024-12-03 17:24:26 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import UserList from './pages/UserList';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Bem-vindo ao App de Usuários</h1>
+    
+      <UserList />
+    </div>
+  );
+};
+
+export default App;
